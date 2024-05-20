c628a29cdecdc5ebd3d6c4b56a60215704380979
sumitjambharkar <sujam3545@gmail.com>
2024-05-20 19:49:42 +0530

    commit

--- model/User.js
+++ model/User.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    userName: {
-      type: String,
-      unique: true,
-    },
     email: {
       type: String,
       unique: true,
